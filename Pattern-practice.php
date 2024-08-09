b055abdce0d066e84af851619929bd5e7686a3b9
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2024-08-09 17:10:50 +0600

    Update Pattern-practice.php

--- Pattern-practice.php
+++ Pattern-practice.php
@@ -1,8 +1,18 @@
 <?php
+
+function PatternZero($number) {
+    for ($i = 1; $i <= $number; $i++) {
+        for ($j = 1; $j <= $number; $j++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+
+}
 // Pattern 1
 function PatternOne($number) {
-    for ($i = 0; $i < $number; $i++) {
-        for ($j = 0; $j <= $i; $j++) {
+    for ($i = 1; $i <= $number; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
             echo "*";
         }
         echo "<br>";
@@ -133,7 +143,7 @@ echo '<br>';
 }
 
  */
-
+// Pattern 8
 function PatternEight($number) {
 
     for ($i = 1; $i <= $number; $i++) {
@@ -156,7 +166,7 @@ function PatternEight($number) {
     }
 
 }
-
+// Pattern 9
 function PatternNine($number) {
     for ($i = 0; $i < $number; $i++) {
         for ($j = 0; $j <= $i; $j++) {
@@ -167,6 +177,7 @@ function PatternNine($number) {
     }
 }
 
+// Pattern 10
 function PatternTen($number) {
     for ($i = 0; $i < $number; $i++) {
         for ($j = 0; $j <= $i; $j++) {
@@ -176,7 +187,7 @@ function PatternTen($number) {
         echo '<br>';
     }
 }
-
+// Pattern 11
 function PatternEleven($number) {
     for ($i = 0; $i <= $number; $i++) {
         for ($j = 0; $j <= $i; $j++) {
@@ -187,6 +198,7 @@ function PatternEleven($number) {
     }
 }
 
+// Pattern 12
 function PatternTwelve($number) {
     for ($i = 0; $i <= $number; $i++) {
         for ($j = 0; $j <= $i; $j++) {
@@ -197,6 +209,7 @@ function PatternTwelve($number) {
     }
 }
 
+// Pattern 13
 function PatternThirteen($number) {
     for ($i = 0; $i <= $number; $i++) {
         for ($j = 0; $j <= $i; $j++) {
@@ -207,6 +220,7 @@ function PatternThirteen($number) {
     }
 }
 
+// Pattern 14
 function PatternForteen($number) {
     for ($i = 1; $i <= $number; $i++) {
         for ($j = $number; $j > $i; $j--) {
@@ -223,6 +237,150 @@ function PatternForteen($number) {
 
 }
 
+// Pattern 15
+function PatternFifteen($number) {
+    for ($i = 1; $i <= $number; $i++) {
+        // Print leading stars
+        for ($j = $i; $j <= $number; $j++) {
+            echo "*";
+        }
+
+        for ($j = 1; $j <= ($i - 1) * 2; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = $i; $j <= $number; $j++) {
+            echo "*";
+        }
+
+        // Move to the next line
+        echo "<br>";
+    }
+
+    for ($i = $number; $i >= 1; $i--) {
+        // Print leading stars
+        for ($j = $i; $j <= $number; $j++) {
+            echo "*";
+        }
+
+        for ($j = 1; $j <= ($i - 1) * 2; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = $i; $j <= $number; $j++) {
+            echo "*";
+        }
+
+        // Move to the next line
+        echo "<br>";
+    }
+
+}
+
+// Pattern 16
+function PatternSixteen($number) {
+    for ($i = 1; $i <= $number; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+
+        for ($j = 1; $j <= ($number - $i) * 2; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+        for ($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+
+        echo "<br>";
+    }
+
+    for ($i = $number; $i > 1; $i--) {
+        for ($j = 1; $j < $i; $j++) {
+            echo "*";
+        }
+
+        for ($j = 1; $j <= ($number - $i + 1) * 2; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+        for ($j = 1; $j < $i; $j++) {
+            echo "*";
+        }
+
+        echo "<br>";
+    }
+}
+
+// Pattern 17
+function PatternSeventeen($number) {
+    for ($i = 1; $i <= $number; $i++) {
+        $num = $i;
+        for ($j = 1; $j <= $i; $j++) {
+            if ($j == 1) {
+                echo $i . "&nbsp;&nbsp;";
+            } else {
+                $num += ($number - $j + 1);
+                echo $num . "&nbsp;&nbsp;";
+            }
+        }
+        echo "<br>";
+    }
+}
+
+// Pattern 18
+function PatternEighteen($number) {
+    for ($i = 1; $i <= $number; $i++) {
+
+        for ($j = 1; $j <= $i; $j++) {
+            if (($j == 1 || $j == $i) || ($i == $number)) {
+                echo "*";
+            } else {
+                echo "&nbsp;&nbsp;";
+            }
+        }
+        echo "<br>";
+    }
+}
+
+// Pattern 19
+function PatternNinteen($number) {
+    $num = 1;
+    for ($i = 1; $i <= $number; $i++) {
+
+        for ($j = 1; $j <= $i; $j++) {
+            echo $num . " ";
+            $num++;
+        }
+
+        echo "<br>";
+    }
+}
+
+// Pattern 20
+
+function PatternTwenty($number) {
+    for ($i = 1; $i <= $number; $i++) {
+        for ($j = 0; $j < $i; $j++) {
+            echo chr(65 + $number - $i + $j) . "&nbsp;&nbsp;&nbsp;";
+        }
+
+        echo "<br>";
+    }
+}
+// Pattern 21
+function PatternTwentyOne($number) {
+    for ($i = 1; $i <= $number; $i++) {
+
+        for ($j = 1; $j <= $number - $i; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = 1; $j <= $i; $j++) {
+            echo $j;
+        }
+
+        echo "<br>";
+    }
+}
 // function PatternForteen($number) {
 //     for ($i = 1; $i <= $number; $i++) {
 //         for ($j = $number; $j > $i; $j--) {
@@ -239,6 +397,101 @@ function PatternForteen($number) {
 
 // }
 
+// Pattern 21
+function PatternTwentyTwo($number) {
+    for ($i = 1; $i <= $number; $i++) {
+
+        for ($j = 1; $j <= $number - $i; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = 1; $j <= $i; $j++) {
+            echo $j;
+        }
+
+        echo "<br>";
+    }
+    for ($i = $number - 1; $i >= 1; $i--) {
+
+        for ($j = 1; $j <= $number - $i; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = 1; $j <= $i; $j++) {
+            echo $j;
+        }
+
+        echo "<br>";
+    }
+}
+
+// Pattern 25
+function PatternTwentyFive($number) {
+    for ($i = 1; $i <= $number; $i++) {
+
+        for ($j = 1; $j <= $number - $i; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = 1; $j <= $i * 2 - 1; $j++) {
+            echo $j;
+        }
+
+        echo "<br>";
+    }
+
+}
+// Pattern 25
+function PatternTwentySix($number) {
+    for ($i = $number; $i >= 1; $i--) {
+
+        for ($j = 1; $j <= $number - $i; $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for ($j = 1; $j <= $i * 2 - 1; $j++) {
+            echo $j;
+        }
+
+        echo "<br>";
+    }
+
+}
+
+// Pattern 27
+
+/*
+function PatternTwentySeven($number) {
+for ($i = 1; $i <= $number; $i++) {
+
+for ($j = 1; $j <= $i; $j++) {
+if (($i % 2 == 1 && $j % 2 == 1) || ($i % 2 == 0 && $j % 2 == 0)) {
+echo 1;
+} else {
+echo 0;
+}
+}
+
+echo "<br>";
+}
+}
+
+ */
+function PatternTwentySeven($number) {
+    for ($i = 1; $i <= $number; $i++) {
+
+        for ($j = 1; $j <= $i; $j++) {
+            if (($i + $j) % 2 == 0) {
+                echo 1;
+            } else {
+                echo 0;
+            }
+        }
+
+        echo "<br>";
+    }
+}
+
 function anotherPattern($number) {
     for ($i = 1; $i <= $number; $i++) {
         for ($j = 1; $j <= $number; $j++) {
@@ -253,6 +506,9 @@ function anotherPattern($number) {
 }
 
 function drowPattern($number) {
+    echo "<h1>Pattern 0</h1>";
+    PatternZero($number);
+    echo "<br>";
     echo "<h1>Pattern 1</h1>";
     PatternOne($number);
     echo "<br>";
@@ -295,37 +551,47 @@ function drowPattern($number) {
     echo "<h1>Pattern 14</h1>";
     PatternForteen($number);
     echo "<br>";
+    echo "<h1>Pattern 15</h1>";
+    PatternFifteen($number);
+    echo "<br>";
+    echo "<h1>Pattern 16</h1>";
+    PatternSixteen($number);
+    echo "<br>";
+    echo "<h1>Pattern 17</h1>";
+    PatternSeventeen($number);
+    echo "<br>";
+    echo "<h1>Pattern 18</h1>";
+    PatternEighteen($number);
+    echo "<br>";
+    echo "<h1>Pattern 19</h1>";
+    PatternNinteen($number);
+    echo "<br>";
+    echo "<h1>Pattern 20</h1>";
+
+    PatternTwenty($number);
+    echo "<br>";
+    echo "<h1>Pattern 21</h1>";
+    PatternTwentyOne($number);
+    echo "<br>";
+    echo "<h1>Pattern 22</h1>";
+    PatternTwentyTwo($number);
+    echo "<br>";
+    echo "<h1>Pattern 25</h1>";
+
+    PatternTwentyFive($number);
+    echo "<br>";
+    echo "<h1>Pattern 26</h1>";
+    PatternTwentySix($number);
+    echo "<br>";
+    echo "<h1>Pattern 27</h1>";
+
+    PatternTwentySeven($number);
+
+    //PatternEighteen($number);
+    echo "<br>";
     echo "<h1>anotherPattern</h1>";
+
     anotherPattern($number);
 }
 
 drowPattern(20);
-// function PatternOw($number) {
-//     for ($i = 1; $i < $number; $i++) {
-//         for ($j = $number; $j > $i; $j--) {
-//             echo "&nbsp&nbsp";
-//         }
-
-//         for ($j = 0; $j < $i; $j++) {
-//             for ($k = 1; $k <= $i + $j; $k++) {
-//                 echo "*";
-//             }
-
-//         }
-
-//         echo "<br>";
-//     }
-
-// }
-// $num = 10;
-// for ($i = 1; $i <= $num; $i++) {
-//     for ($j = 1; $j <= $i; $j++) {
-//         echo "&nbsp&nbsp";
-//     }
-
-//     for ($j = 1; $j <= ($i * 2 - 1); $j++) {
-//         echo "*";
-//     }
-//     echo '<br>';
-
-// }
