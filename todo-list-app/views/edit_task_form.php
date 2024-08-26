0b2e485f5688b271ab766ec53eb9710bd4f450cd
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2024-08-26 17:55:27 +0600

    Delete todo-list-app directory

--- todo-list-app/views/edit_task_form.php
+++
@@ -1,6 +0,0 @@
-<h2>Edit Task</h2>
-<form action="edit_task.php" method="POST">
-    <input type="hidden" name="id" value="<?=$taskData['id']?>">
-    <input type="text" name="title" value="<?=htmlspecialchars($taskData['title'])?>" required>
-    <button type="submit">Update Task</button>
-</form>
